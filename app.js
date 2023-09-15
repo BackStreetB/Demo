fc7fdce03203e3ab965984edb6b380b01c710483
BackStreetB <bethanhbinh1996@gmail.com>
2023-09-15 10:14:51 +0700

    change port from 3000 to 2000

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 2000
 
 app.get('/', (req, res) => {
   res.send('Ayyo Whatsapp')
 })
 
 app.listen(port, () => {
-  console.log(`App Cua Tui lang nghe tren port ${port}`)
+  console.log(`App Cua Tui lang nghe tui tren port ${port}`)
 })
